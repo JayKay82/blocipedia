9d62a79b9e686dba167a72b8fea5aaa075761fea
Jan Kroezen <jankroezen@me.com>
2015-09-29 15:00:17 +0100

    Change XMLHttpRequest location to herokuapp

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,7 +28,7 @@ var blocmetrics = {};
 blocmetrics.report = function(eventName) {
   var event = { name: eventName };
   var request = new XMLHttpRequest();
-  request.open('POST', 'http://localhost:3000/api/events', true);
+  request.open('POST', 'http://jaykay-blocmetrics.herokuapp.com/api/events', true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify(event));
 };
